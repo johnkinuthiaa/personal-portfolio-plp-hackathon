9230d9f6f5c87e7a46a8b5c9f5807a1653539ae6
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-03-31 22:19:46 +0300

    project structure

---
+++ index.js
@@ -0,0 +1,11 @@
+let info =document.getElementById("info")
+const project =()=>{
+
+    return`
+    <div>
+    content
+        <div>another content</div>
+    </div>
+    `
+}
+info.innerHTML +=project()
